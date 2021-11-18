8308c0ebd2c1f9493f618a6cd1b193f31acb7b7d
Artem Biryukov <hazard@live.de>
2021-11-18 08:39:06 +0100

    adds new pdf generate function

--- artem_arbeitsvertrag_cmd.py
+++ artem_arbeitsvertrag_cmd.py
@@ -1,12 +1,11 @@
 import avlib as la
 import os
+from datetime import datetime
 
 mitarbeiterdaten = la.vertragsdaten
 
 la.get_vertrags_input_from_cmd(mitarbeiterdaten)
 la.write_dict_to_configfile(inputdict=mitarbeiterdaten, outputfile="texdata/lat.tex")
-os.system("pdflatex texdata/main.tex")
-
-#noch nicht angepasst
-#outputdir = "texdata/"
-#listoutput = os.listdir( outputdir )
+#os.system("pdflatex texdata/main.tex")
+la.generate_avpdf(configfile="texdata/lat.tex", templatefile="texdata/main.tex", outputdir=".", \
+    jobname=mitarbeiterdaten['nachname']+"_"+mitarbeiterdaten['stellenbezeichnung']+ str(datetime.today().strftime('%Y-%m-%d')))
